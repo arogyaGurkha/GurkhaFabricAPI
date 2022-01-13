ee0f7da0aaa15c0b0f62deda3ba23bc8db60bde8
arogyaGurkha <kharelarogya@hotmail.com>
2022-01-13 20:46:17 +0900

    Update automation.sh for clarity.

--- automation.sh
+++ automation.sh
@@ -2,8 +2,52 @@
 
 DELAY=${10:-"3"}
 
+admin1(){
+  ADMIN1=$(curl --location --request POST '127.0.0.1:8080/fabric/lifecycle/admin/Org1')
+  echo $ADMIN1
+}
+
+admin2(){
+  ADMIN2=$(curl --location --request POST '127.0.0.1:8080/fabric/lifecycle/admin/Org2')
+  echo $ADMIN2
+}
+
+commitCC(){
+  COMMITCC=$(curl --location --request POST '127.0.0.1:8080/fabric/lifecycle/commit' \
+  --header 'Content-Type: application/json' \
+  --data-raw '{
+      "cc_name": "basic",
+      "cc_sequence": 1,
+      "cc_version": "1.0",
+      "channel_name": "mychannel"
+  }')
+  echo $COMMITCC
+}
+
+queryInstall(){
+  INSTALLID=$(curl --location --request GET '127.0.0.1:8080/fabric/lifecycle/install')
+  echo $INSTALLID
+}
+
+installCC(){
+  INSTALLCC=$(curl --location --request POST '127.0.0.1:8080/fabric/lifecycle/install/basic.tar.gz')
+  echo $INSTALLCC
+}
+
+packageCC(){
+  PACKAGE=$(curl --location --request POST '127.0.0.1:8080/fabric/lifecycle/package' \
+  --header 'Content-Type: application/json' \
+  --data-raw '{
+      "cc_source_name": "asset-transfer-basic",
+      "label": "basic_1.0",
+      "language": "go",
+      "package_name": "basic.tar.gz"
+  }')
+  echo $PACKAGE
+}
+
 approveOrg(){
-    APPROVEORG=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/approve' \
+    APPROVEORG=$(curl --location --request POST '127.0.0.1:8080/fabric/lifecycle/approve' \
     --header 'Content-Type: application/json' \
     --data-raw '{
         "cc_name": "basic",
@@ -17,7 +61,7 @@ approveOrg(){
 
 queryCommitReady(){
      sleep $DELAY
-    QUERYCOMMITREADY=$(curl --location --request GET '15.164.97.167:8080/fabric/lifecycle/commit/organizations' \
+    QUERYCOMMITREADY=$(curl --location --request GET '127.0.0.1:8080/fabric/lifecycle/commit/organizations' \
     --header 'Content-Type: application/json' \
     --data-raw '{
     "cc_name": "basic",
@@ -30,7 +74,7 @@ queryCommitReady(){
 
 queryCommitted(){
      sleep $DELAY
-    QUERYCOMMIT=$(curl --location --request GET '15.164.97.167:8080/fabric/lifecycle/commit' \
+    QUERYCOMMIT=$(curl --location --request GET '127.0.0.1:8080/fabric/lifecycle/commit' \
     --header 'Content-Type: application/json' \
     --data-raw '{
     "cc_name": "basic",
@@ -40,85 +84,51 @@ queryCommitted(){
 }
 
 ## Package the CC "asset-transfer-basic"
-PACKAGE=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/package' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "cc_source_name": "asset-transfer-basic",
-    "label": "basic_1.0",
-    "language": "go",
-    "package_name": "basic.tar.gz"
-}')
-echo $PACKAGE
+packageCC
 
 ## Set Org1 as the admin
-ADMIN1=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/admin/Org1')
-echo $ADMIN1
-
+admin1
 ## Install for Org1
-INSTALLCC=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/install/basic.tar.gz')
-echo $INSTALLCC
+installCC
 
 ## Set Org2 as the admin
-ADMIN2=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/admin/Org2')
-echo $ADMIN2
-
+admin2
 ## Install for Org2
-INSTALLCC=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/install/basic.tar.gz')
-echo $INSTALLCC
+installCC
 
 ## Set Org1 as the admin
-ADMIN1=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/admin/Org1')
-echo $ADMIN1
-
+admin1
 ## Check if installed succesfully
-INSTALLID=$(curl --location --request GET '15.164.97.167:8080/fabric/lifecycle/install')
-echo $INSTALLID
+queryInstall
 
 ## Approve for Org1
-ADMIN1=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/admin/Org1')
-echo $ADMIN1
+admin1
 approveOrg
 
 ## Check commit readiness from both Orgs
-ADMIN1=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/admin/Org1')
-echo $ADMIN1
+admin1
 queryCommitReady
 
-ADMIN2=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/admin/Org2')
-echo $ADMIN2
+admin2
 queryCommitReady
 
 ## Approve for Org2
-ADMIN2=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/admin/Org2')
-echo $ADMIN2
+admin2
 approveOrg
 
 ## Check commit readiness from both Orgs
-ADMIN1=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/admin/Org1')
-echo $ADMIN1
+admin1
 queryCommitReady
 
-ADMIN2=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/admin/Org2')
-echo $ADMIN2
+admin2
 queryCommitReady
-
 ## Commit the definition
-COMMITCC=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/commit' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "cc_name": "basic",
-    "cc_sequence": 1,
-    "cc_version": "1.0",
-    "channel_name": "mychannel"
-}')
-echo $COMMITCC
+commitCC
 
 ## Check committed CC from both Orgs
-ADMIN1=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/admin/Org1')
-echo $ADMIN1
+admin1
 queryCommitted
 
 ## Check committed CC from both Orgs
-ADMIN2=$(curl --location --request POST '15.164.97.167:8080/fabric/lifecycle/admin/Org1')
-echo $ADMIN2
+admin2
 queryCommitted
